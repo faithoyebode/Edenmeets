7b687216b36d26d62d199deb5f132f233af99448
Faith <oyebodefaith3@gmail.com>
2020-12-29 17:46:26 +0100

    sign in page created but carousel slider and dropdown input from previous commit are not yet resolved

--- src/components/pages/unAuthPages/SignIn.tsx
+++ src/components/pages/unAuthPages/SignIn.tsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React, { FC, ReactElement } from 'react';
+import styled from '@emotion/styled';
+import AuthTemplate from "components/reusables/AuthTemplate";
+import { Link } from 'react-router-dom';
 
-const SignIn = () => {
+
+const SignInWrapper = styled.div`
+    .f-14{
+        font-size: 14px;
+    }
+    .form-section{
+        background: rgba(40, 40, 40, 0.67);
+        box-shadow: 0px 4px 15px -3px rgba(0, 0, 0, 0.16);
+        border-radius: 8px;
+        align-self: flex-end;
+        color: #ffffff;
+        max-width: 444px;
+        padding: 48px 37px 65px 37px;
+
+        form{
+            .header{
+                font-size: 25px;
+            }
+            .group{
+                margin-bottom: 23px;
+                
+                input{
+                    width: 100%;
+                    border: none;
+                    padding: 12px 20px;
+                    height: 40px;
+                    background: #FFFFFF;
+                    box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1);
+                    border-radius: 8px;
+
+                    &:hover{
+                        border: 1px solid #F34E4E;
+                        outline: none;
+                    }
+                    &:focus{
+                        border: 1px solid #F34E4E;
+                        outline: none; 
+                    }
+                }
+            }
+            button{
+                border: none;
+                color: #ffffff;
+                border-radius: 40px;
+                height: 50px;
+                margin-bottom: 24px;
+            }
+            .fgt-pwd, .sup{
+                all: unset;
+                cursor: pointer;
+                color: #F34E4E;
+            }
+        }
+    }
+    .sup-directive{
+        font-size: 20px;
+        margin-top: 18px;
+    }
+
+`;
+
+
+const SignIn: FC<any>= (): ReactElement => {
     return (
-        <div>
-            SignIn
-        </div>
+        <SignInWrapper>
+            <AuthTemplate>
+                <div className="form-section">
+                    <form>
+                        <div className="header d-flex justify-content-between">
+                            <p>Sign in to Edenmeets...</p>
+                            <p>&#10005;</p>
+                        </div>
+                        <p className="f-14">Please enter your sign in details</p>
+                        <div className="group">
+                            <label className="d-block">Email address / Phone Number</label>
+                            <input type="text" placeholder="Email / Phone" name="" id="" />
+                        </div>
+                        <div className="group">
+                            <label className="d-block">Password</label>
+                            <input type="password" placeholder="Enter password" name="" id="" />
+                        </div>
+                        
+                        <button type="submit" className="d-block w-100 pry-button">Sign me in 🤚</button>
+                        <Link to="/forgot-password" className="f-14 fgt-pwd">Forgot password</Link>
+                        <p className="sup-directive">
+                            <span><Link to="/sign-up" className="sup">Sign Up</Link></span> here if you are not yet directed
+                        </p>
+                    </form>
+                </div>
+            </AuthTemplate>
+        </SignInWrapper>
     )
 }
 
-export default SignIn
+export default SignIn;
---
+++ src/components/reusables/AuthTemplate.tsx
@@ -0,0 +1,81 @@
+import React, { FC, ReactElement } from 'react';
+import styled from '@emotion/styled';
+import HeroImg from 'assets/images/auth-hero.jpg';
+import NavBar from "components/reusables/NavBar";
+import Footer from "components/reusables/Footer";
+
+
+
+
+
+
+const AuthTemplateWrapper = styled.div`
+    
+    .hero{
+        min-height: 1049px;
+        background: linear-gradient(104.88deg, rgba(0, 0, 0, 0.9) -5.41%, rgba(0, 0, 0, 0) 102.1%), url(${HeroImg});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: 30% top;
+        margin-bottom: 100px;
+        padding-bottom: 91px;
+
+        .row{
+            align-items: center;
+            height: 70%;
+            margin-top: 45px;
+
+            .col-lg-6{
+                
+            }
+        }
+        .hero__theme{
+            color: #ffffff;
+            font-family: "Montserrat-Bold";
+            font-size: 64px;
+        }
+    }
+    
+    @media all and (max-width: 768px){
+        .row{
+            align-items: flex-start !important;
+        }
+        .hero__theme{
+            font-size: 40px !important;
+        }
+    }
+    @media all and (max-width: 425px){
+        .hero__theme{
+            margin-bottom: 14px !important;
+        }
+    }
+
+`;
+
+interface AuthChildProps{
+    children: FC<any>;
+}
+
+
+const AuthTemplate: FC<any> = ({ children }: AuthChildProps ): ReactElement => {
+    return (
+        <AuthTemplateWrapper>
+            <section className="hero position-relative">
+                <div className="container h-100">
+                    <NavBar />
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6">
+                            <h3 className="hero__theme">Your true christian love lives here</h3>
+                        </div>
+                        <div className="col-lg-6 col-md-6">
+                           { children }
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </AuthTemplateWrapper> 
+    )
+}
+
+export default AuthTemplate;
